a83cb5dbdfa9b2cf471ad596ec7a8397df509e0e
dajeong <dajeong@crscube.co.kr>
2019-10-06 01:25:27 +0900

    Event Subscriptions 을 위한 Verification API 추가

--- src/main/java/com/oktop/bot/BotController.java
+++ src/main/java/com/oktop/bot/BotController.java
@@ -2,8 +2,11 @@ package com.oktop.bot;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bot")
@@ -12,11 +15,18 @@ public class BotController {
 
     private final NoticeService noticeService;
 
-    @PostMapping("/call")
-    public String testBot() {
+    @PostMapping("/webhook")
+    public String webhook() {
         Notice notice = new Notice();
         notice.setText("Hello World !");
         noticeService.notice(notice);
         return "success";
+    }
+
+    @PostMapping("event")
+    public String interactivity(@RequestBody Map<String, Object> event) {
+        System.out.println(event.toString());
+        String challenge = (String) event.get("challenge");
+        return challenge;
     }
 }
